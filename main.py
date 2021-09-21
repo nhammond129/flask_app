86fc3ae8337ff71a9350d4d50bf064c844c5a87d
Nicholas Hammond <nhammond129@gmail.com>
2021-09-20 20:16:43 -0500

    rename main

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import flask
 app = flask.Flask(__name__, template_folder='templates')
 
 @app.route("/")
-@app.route("/index.html")
-def hello_world():
+def index():
     return flask.render_template("index.html")
 
 if __name__ == "__main__":
