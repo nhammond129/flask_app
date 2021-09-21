27379172a110fb846b2d65f4bc2c2ae6781a8fc2
Nicholas Hammond <nhammond129@gmail.com>
2021-09-20 23:53:16 -0500

    add chart

---
+++ backend/data.py
@@ -0,0 +1,47 @@
+import random
+import time
+import json
+from threading import Thread
+from typing import Callable
+from datetime import datetime
+
+class DataGenerator:
+    class Subscriber:
+        def __init__(self, parent):
+            self.queue = []
+            self.parent = parent
+            self.parent.subscribers.add(self)
+        def write(self, val):
+            self.queue.append(val)
+        def read(self):
+            while True:
+                if len(self.queue):
+                    yield self.queue.pop(0)
+        def kill(self):
+            self.parent.remove(self)
+
+    def __init__(self):
+        self.subscribers = set()
+        self.thread = Thread(target=self.runner)
+        self.thread.start()
+    
+    def runner(self):
+        value = random.uniform(0, 100)
+        while True:
+            value += random.uniform(-1,1)
+            if value > 100: value = 100
+            if value < 0: value = 0
+
+            json_data = json.dumps({
+                'time': datetime.now().strftime('%H:%M:%S'),
+                'value': value
+                })
+            for sub in self.subscribers:
+                sub.write(f"data:{json_data}\n\n")
+
+            time.sleep(0.1)
+
+    def subscribe(self):
+        return self.Subscriber(self)
+
+generator = DataGenerator()
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import flask
+import backend
+from threading import Thread
 
-app = flask.Flask(__name__, template_folder='templates')
+app = flask.Flask(__name__, template_folder='templates', static_folder='static', static_url_path='')
 
 @app.route("/")
 def index():
@@ -8,12 +10,23 @@ def index():
 
 @app.route("/post")
 def post():
-	return flask.render_template("post.html")
+    return flask.render_template("post.html")
 
 @app.route("/request", methods=['POST'])
 def request():
-	name = flask.request.form['name']
-	return flask.render_template("request.html", name=name)
+    name = flask.request.form['name']
+    return flask.render_template("request.html", name=name)
+
+@app.route("/data")
+def data():
+    return flask.render_template("data.html")
+
+import time, json
+from datetime import datetime
+@app.route('/data-sse-stream')
+def chart_data():
+    sub = backend.generator.subscribe()
+    return flask.Response(sub.read(), mimetype='text/event-stream')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(threaded=True)
