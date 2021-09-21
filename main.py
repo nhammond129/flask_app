57f094d3bceb20d1263b125f459688eb6c5c7948
Nicholas Hammond <nhammond129@gmail.com>
2021-09-20 20:17:07 -0500

    add request, post

--- main.py
+++ main.py
@@ -6,5 +6,15 @@ app = flask.Flask(__name__, template_folder='templates')
 def index():
     return flask.render_template("index.html")
 
+@app.route("/post")
+def post():
+	return flask.render_template("post.html")
+
+@app.route("/request", methods=['POST'])
+def request():
+	name = flask.request.form['name']
+	return flask.render_template("request.html", name=name)
+
+
 if __name__ == "__main__":
     app.run()
